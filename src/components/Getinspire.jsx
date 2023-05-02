f6d1420fdc0e3fcfb4c0ce20ea5dfef898493c75
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-02 20:47:00 +0600

    another resonable section is added

--- src/components/Getinspire.jsx
+++ src/components/Getinspire.jsx
@@ -34,6 +34,40 @@ const Getinspire = () => {
 </div>
   </div>
   
+
+  <h2 className="text-2xl text-center text-green-600">Special Discount on this Month</h2>
+
+  <div className="grid md:grid-cols-3 grid-cols-1 p-6" >
+  
+  <div className="card border-2 w-80 ">
+  <div className="card-body ">
+    <img className="rounded-2xl border-2 p-2" src="https://c8.alamy.com/zooms/9/c8496a064fc5458ab9e05341cd2054d2/khtj31.jpg" alt="" />
+    <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+ 
+ 
+  <div className="card border-2 w-80">
+  <div className="card-body ">
+    <img className="rounded-2xl border-2 p-2" src="https://cdn.w600.comps.canstockphoto.com/bangla-cuisine-vorta-vaji-fish-curry-stock-photos_csp52483333.jpg" alt="" />
+    <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+  
+  <div className="card border-2 w-80">
+  <div className="card-body ">
+    <img className="rounded-2xl border-2 p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRji870hMQ2pCL3BwANtixhfHg7ZlrAxTb-3AzPi4Nn0v0x_F6jLJ2GpV0beqGheh-okMw&usqp=CAU" alt="" />
+    <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+  </div>
+  
   
     </div>
   );
--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 
 import app from '../../firebase/firebase.config';
 
@@ -10,14 +10,30 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
    const [user,setUser] = useState(null)
+   const [loading,setLoading] = useState(true)
 
    const createUser = (email,password)=>{
+    setLoading(true)
     return createUserWithEmailAndPassword(auth,email,password)
    }
 
    const signIn = (email,password)=> {
+    setLoading(true)
     return signInWithEmailAndPassword(auth,email,password)
    }
+
+   useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth,loggedUser => {
+           setUser(loggedUser)
+        //    setLoading(false)
+
+       })
+
+       return ()=> {
+           unsubscribe()
+       }
+
+   },[])
    
 
    const authInfo = {
