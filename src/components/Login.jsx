24f4b33569fda6862e7eb381beb9b98592365ebc
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-02 16:41:45 +0600

    register and login is added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is Login</h2>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col ">
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">Please Login now!</h1>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" className="input input-bordered" />
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" className="input input-bordered" />
+          <label className="label">
+           
+             <p> Create New Account ? <Link className='underline' to='/register'>
+             Register
+                </Link>  </p>
+           
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+
+        <div className=' text-white border-2 p-2 rounded-lg bg-slate-950 flex items-center mx-auto w-full gap-2'> <FaGoogle></FaGoogle> 
+         <span>Login With Google</span>
+         </div>
+
+        <div className=' text-white border-2 p-2 rounded-lg bg-slate-950 flex items-center mx-auto w-full gap-2'>
+        <FaGithub></FaGithub>
+         <span>Login With GitHub</span>
+         </div>
+
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>this is Register</h2>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col ">
+    <div className="text-center ">
+      <h1 className="text-2xl font-bold">Please Register now!</h1>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+   
+      <div className="card-body">
+     
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name='text' placeholder="name" className="input input-bordered" />
         </div>
+     
+     
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo Url</span>
+          </label>
+          <input type="text" name='photo' placeholder="photo" className="input input-bordered" />
+        </div>
+     
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+        </div>
+     
+    
+    
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name='password' placeholder="password" className="input input-bordered" />
+          <label className="label">
+            <p>Already have an account ? <Link className='underline' to='/login'>Login</Link> </p>
+          </label>
+        </div>
+       
+       
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
