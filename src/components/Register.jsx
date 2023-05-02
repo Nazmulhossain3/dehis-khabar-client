56590f402dc5667adb1e9c63f12b2c040a295829
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-02 16:51:05 +0600

    register data get from input

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+
+    
+    const handleSubmit = (event)=> {
+        event.preventDefault()
+        const form = event.target 
+        const name = form.name.value 
+        const photo = form.photo.value
+        const email = form.email.value 
+        const password = form.password.value 
+        console.log(name,photo,email,password)
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col ">
@@ -10,13 +23,13 @@ const Register = () => {
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
    
-      <div className="card-body">
+      <form onSubmit={handleSubmit} className="card-body">
      
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="text" name='text' placeholder="name" className="input input-bordered" />
+          <input type="text" name='name' placeholder="name" className="input input-bordered" />
         </div>
      
      
@@ -50,7 +63,7 @@ const Register = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Register</button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
