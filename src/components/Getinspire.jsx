d4c590d7a90d5972ddf6c2761503c1bc31968122
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-03 20:30:38 +0600

    responsive done

--- src/components/Getinspire.jsx
+++ src/components/Getinspire.jsx
@@ -8,26 +8,32 @@ const Getinspire = () => {
 
   <div className="grid md:grid-cols-3 grid-cols-1 p-6" >
   
-  <div className="card w-96">
+  <div className="card">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://www.upoharbd.com/images/uploads/Food/Nobabi_vog_Dhaka.jpg" alt="" />
+    <p className="text-orange-500">Name : Hyderabadi Biryani</p>
+    <button className="text-white w-full bg-orange-500 p-2 rounded-2xl btn-outline">Order Now</button>
     <div className="card-actions justify-end">
     </div>
   </div>
 </div>
  
  
-  <div className="card w-96">
+  <div className="card">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://static.hungrynaki.com/hungrynaki-v4/restaurants/kacchi_bhai/items/kacchi_bhai_kacchi_khadok_thumbnail_1649580256406.jpeg" alt="" />
+   <p className="text-orange-500">Name : Awadhi Mutton Biryani</p>
+   <button className="text-white w-full bg-orange-500 p-2 rounded-2xl btn-outline">Order Now</button>
     <div className="card-actions justify-end">
     </div>
   </div>
 </div>
   
-  <div className="card w-96">
+  <div className="card">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://static.hungrynaki.com/hungrynaki-v4/restaurants/kacchi_bhai/items/kacchi_bhai_basmati_kacchi_for_5_persons_thumbnail_1649581091345.jpeg" alt="" />
+    <p className="text-orange-500"> Name : Makhni Paneer Biryani</p>
+    <button className="text-white w-full bg-orange-500 p-2 rounded-2xl btn-outline">Order Now</button>
     <div className="card-actions justify-end">
     </div>
   </div>
@@ -39,7 +45,7 @@ const Getinspire = () => {
 
   <div className="grid md:grid-cols-3 grid-cols-1 p-6" >
   
-  <div className="card border-2 w-80 ">
+  <div className="card border-2  ">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://c8.alamy.com/zooms/9/c8496a064fc5458ab9e05341cd2054d2/khtj31.jpg" alt="" />
     <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
@@ -49,7 +55,7 @@ const Getinspire = () => {
 </div>
  
  
-  <div className="card border-2 w-80">
+  <div className="card border-2 ">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://cdn.w600.comps.canstockphoto.com/bangla-cuisine-vorta-vaji-fish-curry-stock-photos_csp52483333.jpg" alt="" />
     <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
@@ -58,7 +64,7 @@ const Getinspire = () => {
   </div>
 </div>
   
-  <div className="card border-2 w-80">
+  <div className="card border-2">
   <div className="card-body ">
     <img className="rounded-2xl border-2 p-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRji870hMQ2pCL3BwANtixhfHg7ZlrAxTb-3AzPi4Nn0v0x_F6jLJ2GpV0beqGheh-okMw&usqp=CAU" alt="" />
     <p>Vorta,baji, Fish curry : 30% Off</p> <button className="w-full bg-green-400 p-2 rounded-2xl btn-outline">Order Now</button>
